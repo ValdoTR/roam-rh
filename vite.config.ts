eb34177c4d20e9a58f68bf4c5174d28a87b3d486
Valdo <vteixeiraromao@gmail.com>
2023-01-03 19:28:01 +0100

    Added teleport website

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
+                jobs: "./jobs.html",
                 ...getMapsScripts(),
             },
         },
