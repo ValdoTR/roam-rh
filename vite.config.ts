e8aaa51659437d115437c538b8b3e81a56d8a96f
Valdo <vteixeiraromao@gmail.com>
2023-01-26 16:07:31 +0000

    Fixed unavailable.html ref

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
             input: {
                 index: "./index.html",
                 jobs: "./jobs.html",
-                available: "./available.html",
+                available: "./unavailable.html",
                 ...getMapsScripts(),
             },
         },
